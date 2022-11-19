d96c71f700812ab6c6d5ba13a585c211a4205020
polynomialspace <github@polynomial.space>
2022-11-18 22:07:48 -0800

    init

---
+++ bytesize.go
@@ -0,0 +1,19 @@
+package bytesize
+
+const (
+	KB = 1000
+	MB = KB * 1000
+	GB = MB * 1000
+	TB = GB * 1000
+	PB = TB * 1000
+	EB = PB * 1000
+)
+
+const (
+	KiB = 1024
+	MiB = KiB * 1024
+	GiB = MiB * 1024
+	TiB = GiB * 1024
+	PiB = TiB * 1024
+	EiB = PiB * 1024
+)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/polynomialspace/bytesize
+
+go 1.19
